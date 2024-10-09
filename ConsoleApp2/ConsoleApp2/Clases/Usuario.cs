7606dd51391158dc516f097337c13083bbe5f030
agustin-cabrera <126021549+Agustin1881@users.noreply.github.com>
2024-10-09 11:39:31 -0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Clases/Usuario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Clases
+{
+    public class Usuario
+    {
+        public string NombreUsuario { get; set; }
+        public string Contrasena { get; set; }
+        public int CantFichas { get; set; }
+
+        public Usuario(string nombreUsuario, string contrasena, int cantFichas)
+        {
+            NombreUsuario = nombreUsuario;
+            Contrasena = contrasena;
+            CantFichas = cantFichas;
+        }
+
+        public Usuario(string nombreUsuario, string contrasena)
+        {
+            NombreUsuario = nombreUsuario;
+            Contrasena = contrasena;
+        }
+
+        public void MostrarInfo()
+        {
+            Console.WriteLine($"Usuario: {NombreUsuario}" +
+                $"\nContraseña: {Contrasena}" +
+                $"\nCantidad de Fichas: {CantFichas}\n");
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,117 @@
+﻿using ConsoleApp2.Clases;
+
+class Program
+{
+
+    static void Main()
+    {
+        void LogIn(int numPlayer, string user, string password, List<Usuario> ListaUsuarios,
+        string usuarioActual, string contraActual)
+        {
+            for (int i = 0; i < ListaUsuarios.Count; i++)
+            {
+                if (ListaUsuarios[i].NombreUsuario == user &&
+                    ListaUsuarios[i].Contrasena == password)
+                {
+                    usuarioActual = user;
+                    contraActual = password;
+                    numPlayer = i;
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Usuario o contraseña incorrecto");
+                }
+            }
+        }
+
+
+        List<Usuario> ListaUsuarios = new List<Usuario>();
+        bool Registrado = false;
+        int opcionGame, numPlayer = 0;
+        string usuarioActual = "", contraActual = "";
+
+        while (!Registrado)
+        {
+            Console.Clear();
+            Console.WriteLine("\n=== Casino Web ===\n\n" +
+                              "1. Sign In\n" +
+                              "2. Log In");
+            int opcionMenu = int.Parse(Console.ReadLine());
+
+            switch (opcionMenu)
+            {
+                case 1:
+                    Console.Clear();
+                    Console.Write("Nuevo usuario: ");
+                    string nuevoUser = Console.ReadLine();
+                    Console.Write("Nueva contraseña: ");
+                    string nuevaContra = Console.ReadLine();
+                    Console.Write("Confirme contraseña: ");
+                    string nuevaContraVerificacion = Console.ReadLine();
+
+                    if (nuevaContra == nuevaContraVerificacion)
+                    {
+                        ListaUsuarios.Add(new Usuario(nuevoUser, nuevaContra, 2000));
+                        Console.WriteLine("Usuario registrado exitosamente.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Las contraseñas no coinciden.");
+                    }
+                    break;
+
+                case 2:
+                    Console.Clear();
+                    Console.Write("\nUsuario: ");
+                    string user = Console.ReadLine();
+                    Console.Write("Contraseña: ");
+                    string contra = Console.ReadLine();
+
+                    LogIn(numPlayer, user, contra, ListaUsuarios, usuarioActual, contraActual);
+                    Registrado = true;
+                    
+                    break;
+                   
+                default:
+                    Console.WriteLine("Opcion no valida\n" +
+                                      "Ingrese otra opcion");
+                    Console.Clear();
+                    break;
+            }
+        }
+
+        while (Registrado)
+        {
+            Console.Clear();
+            Console.WriteLine("\nBienvenido!\n");
+            ListaUsuarios[numPlayer].MostrarInfo();
+            Console.WriteLine("Juegos:" +
+                                          "\n1_ 21 BlackJack" +
+                                          "\n2_ Poker" +
+                                          "\n3_ Tragamonedas");
+            opcionGame = int.Parse(Console.ReadLine());
+
+            switch (opcionGame)
+            {
+                case 1:
+                    Console.Clear();
+                    Console.WriteLine("\nJuego: 21 BlackJack");
+                    break;
+
+                case 2:
+                    Console.Clear();
+                    Console.WriteLine("\nJuego: Poker");
+                    break;
+
+                case 3:
+                    Console.Clear();
+                    Console.WriteLine("\nJuego: Tragamonedas");
+                    break;
+                default:
+                    Console.WriteLine("Opción de categoría no válida.");
+                    break;
+            }
+        }
+    }
+}
